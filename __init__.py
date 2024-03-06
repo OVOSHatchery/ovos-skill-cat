8fc6c0b6b6a3e41ff5cd8a14dea0a748ae88cc3d
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-03-06 18:40:02 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-from mycroft import MycroftSkill, intent_file_handler
+from ovos_workshop.skills import OVOSSkill
+from ovos_workshop.decorators import intent_handler
 import requests
 
-from mycroft.skills.core import resting_screen_handler
+from ovos_workshop.decorators import resting_screen_handler
 
 
-class CatSkill(MycroftSkill):
+class CatSkill(OVOSSkill):
+
     def update_cat(self):
         r = requests.get('https://api.thecatapi.com/v1/images/search')
         if r:
@@ -15,13 +17,9 @@ class CatSkill(MycroftSkill):
         self.update_cat()
         self.gui.show_page('idle.qml')
 
-    @intent_file_handler('ShowCat.intent')
+    @intent_handler('ShowCat.intent')
     def cat_handler(self, message):
         self.log.info('SHOWING CAT')
         self.update_cat()
         self.gui.show_page('cat.qml')
         self.speak_dialog('mjau')
-
-
-def create_skill():
-    return CatSkill()
